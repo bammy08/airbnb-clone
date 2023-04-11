60fdc6ad9db8d805a903f9b66ab9947dd46ceac6
bammy08 <olusolabamidele33@gmail.com>
2023-04-11 14:17:07 +0100

    created the listing component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,47 @@
+import { useSearchParams } from 'next/navigation';
+import getListings, { IListingsParams } from './actions/getListings';
+import Container from './components/Container';
+import EmptyState from './components/EmptyState';
+import ListingCard from './components/listings/ListingCard';
+import getCurrentUser from './actions/getCurrentUser';
 
-
-export default function Home() {
-  return (
-   <div className=' text-red-500'>
-    Hello world
-   </div>
-  )
+interface HomeProps {
+  searchParams: IListingsParams;
 }
+
+const Home = async ({ searchParams }: HomeProps) => {
+  const currentUser = await getCurrentUser();
+  const listings = await getListings(searchParams);
+
+  if (listings.length === 0) {
+    return <EmptyState showReset />;
+  }
+  return (
+    <Container>
+      <div
+        className="
+            pt-24
+            grid 
+            grid-cols-1 
+            sm:grid-cols-2 
+            md:grid-cols-3 
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+            gap-8
+          "
+      >
+        {listings.map((listing: any) => (
+          <ListingCard
+            currentUser={currentUser}
+            key={listing.id}
+            data={listing}
+          />
+        ))}
+        <div>My listings</div>
+      </div>
+    </Container>
+  );
+};
+
+export default Home;
